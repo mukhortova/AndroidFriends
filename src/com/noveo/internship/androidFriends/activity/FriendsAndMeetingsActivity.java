2c104a6244f9b8456517da733861c6f2cfa37e50
batalov.vlad <Inverse12@gmail.com>
2012-07-18 23:24:32 +0700

    Added tabs and menu, perhaps all right

---
+++ src/com/noveo/internship/androidFriends/activity/FriendsAndMeetingsActivity.java
@@ -0,0 +1,10 @@
+package com.noveo.internship.androidFriends.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FriendsAndMeetingsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
